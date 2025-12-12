3237a00689b20b2ec2adcc28f8d23c7fb5d8390c
KumarThing <102548843+KumarThing@users.noreply.github.com>
2025-12-12 17:38:44 +0200

    main.go

--- main.go
+++ main.go
@@ -55,6 +55,27 @@ func main() {
 
 	http.HandleFunc("/showall" , func(w http.ResponseWriter, r *http.Request){
 		showallTmpl.Execute(w, Expenses)
+
+	})
+
+	http.HandleFunc("/delete", func(w http.ResponseWriter, r *http.Request){
+		if r.Method == http.MethodPost{
+			r.ParseForm()
+
+			delStr := r.FormValue("delete")
+
+			i,err:= strconv.Atoi(delStr)
+
+			if err != nil {
+				http.Redirect(w, r, "/showall", http.StatusSeeOther)
+				return
+			}
+			if i >= 0 && i <len(Expenses) {
+				Expenses = append(Expenses[:i], Expenses[i+1:]... )
+			}
+			http.Redirect(w, r, "/showall", http.StatusSeeOther)
+			return
+		}
 	})
 
 	fmt.Println("Your server is running in http://localhost:8080")
